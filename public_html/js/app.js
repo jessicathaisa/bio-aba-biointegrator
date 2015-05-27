876b10f423708ecb8cb40dd27cade9564a080424
Jessica Thaisa Silva de Almeida <jessicatsalmeida@gmail.com>
2015-05-27 11:06:54 -0300

    melhoria na visualização. Chamada para criação de task criada. Guardando taskKey no coockie do cliente.

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,56 +1,108 @@
-
-
-var app = angular.module('bioIntegrator', ['controllers','ngTranslate']);
+var app = angular.module('bioIntegrator', ['controllers', 'ngTranslate']);
 var controllers = angular.module('controllers', []);
 
-controllers.controller('Hello', Hello);
+controllers.controller('RunningTask', RunningTask);
+controllers.controller('BioTask', BioTask);
 controllers.controller('SwitchLanguageController', SwitchLanguageController);
 controllers.controller('HeaderController', HeaderController);
 
-function Hello($scope, $http) {
+function RunningTask($scope, $cookieStore) {
+    $scope.lastTaskKey = $cookieStore.get('LAST_TASKKEY');
+}
+;
+
+function BioTask($scope, $http, $cookieStore) {
+    $scope.loadingDatabase = false;
+    $scope.loadingAlgorithm = false;
     $http.get('http://localhost:8080/taskmanager-web/rest/biodatabase').
             success(function (data) {
                 $scope.biodatabases = data;
                 $scope.selectedDB = "";
+                $scope.loadingDatabase = true;
             });
-};
-
-function SwitchLanguageController ($translate, $scope) {
-    $scope.toggleLang = function () {
-        $translate.uses() === 'en_EN'? $translate.uses('pt_BR') : $translate.uses('en_EN');
+    $http.get('http://localhost:8080/taskmanager-web/rest/bioalgorithm').
+            success(function (data) {
+                $scope.bioalgorithms = data;
+                $scope.selectedAlg = "";
+                $scope.loadingAlgorithm = true;
+            });
+    $scope.submit = function () {
+        $http.post('http://localhost:8080/taskmanager-web/rest/biotask', {database: $scope.selectedDB, algorithm: $scope.selectedAlg}).
+                success(function (data) {
+                    $scope.task = data;
+                    $cookieStore.put('LAST_TASKKEY', $scope.task.taskKey);
+                    alert($scope.task.taskKey);
+                });
     };
-};
+}
+;
 
-function HeaderController($scope, $location) 
-{ 
-    $scope.isActive = function (viewLocation) { 
+function SwitchLanguageController($translate, $scope) {
+    $scope.toggleLang = function () {
+        $translate.uses() === 'en_EN' ? $translate.uses('pt_BR') : $translate.uses('en_EN');
+    };
+}
+;
+
+function HeaderController($scope, $location)
+{
+    $scope.isActive = function (viewLocation) {
         return viewLocation === $location.path();
     };
-};
+}
+;
 
 // Configuring $translateProvider
 app.config(['$translateProvider', function ($translateProvider) {
-    
-    // Simply register translation table as object hash
-    $translateProvider.translations('en_EN', {
-        'TITLE': 'Bio-ABA - The new Integrator between Applications, Biological databases and algorithms',
-        'SIMPLE_TITLE': 'Bio-ABA',
-        'SIMPLE_SUBTITLE': 'The new Integrator between Applications, Biological databases and algorithms.',
-        'HOME': 'Home',
-        'HELP': 'Help',
-        'CONTACT': 'Contact Us'
-    });  
-    // Simply register translation table as object hash
-    $translateProvider.translations('pt_BR', {
-        'TITLE': 'Bio-ABA - Um novo integrador entre Aplicações, bancos de dados e algoritmos da biologia molecular',
-        'SIMPLE_TITLE': 'Bio-ABA',
-        'SIMPLE_SUBTITLE': 'Um novo integrador entre Aplicações, bancos de dados e algoritmos da biologia molecular.',
-        'HOME': 'Página Inicial',
-        'HELP': 'Ajuda',
-        'CONTACT': 'Contate-nos'
-    });   
-    
-    $translateProvider.uses('en_EN');
-    $translateProvider.rememberLanguage(true);
-    
-}]);
+
+        // Simply register translation table as object hash
+        $translateProvider.translations('en_EN', {
+            'TITLE': 'Bio-ABA - The new Integrator between Applications, Biological databases and algorithms',
+            'SIMPLE_TITLE': 'Bio-ABA',
+            'SIMPLE_SUBTITLE': 'The new Integrator between Applications, Biological databases and algorithms.',
+            'HOME': 'Home',
+            'HELP': 'Help',
+            'CONTACT': 'Contact Us',
+            'DATABASETITLE_LABEL': 'Choose a database to use:',
+            'DATABASETITLE_SELECT': '-- Choose the database --',
+            'DATABASENAME_LABEL': 'Name',
+            'DATABASEDESCRIPTION_LABEL': 'Description',
+            'DATABASEFORMAT_LABEL': 'Format',
+            'ALGORITHMTITLE_LABEL': 'Choose an algorithm to execute:',
+            'ALGORITHMTITLE_SELECT': '-- Choose the algorithm --',
+            'ALGORITHMNAME_LABEL': 'Name',
+            'ALGORITHMDESCRIPTION_LABEL': 'Description',
+            'ALGORITHMPROVIDER_LABEL': 'Provider',
+            'ALGORITHMPROVIDER_NULL' : "Not informed provider",
+            
+            
+            'SENT_TASK' : "Sent Task"
+        });
+        // Simply register translation table as object hash
+        $translateProvider.translations('pt_BR', {
+            'TITLE': 'Bio-ABA - Um novo integrador entre Aplicações, bancos de dados e algoritmos da biologia molecular',
+            'SIMPLE_TITLE': 'Bio-ABA',
+            'SIMPLE_SUBTITLE': 'Um novo integrador entre Aplicações, bancos de dados e algoritmos da biologia molecular.',
+            'HOME': 'Página Inicial',
+            'HELP': 'Ajuda',
+            'CONTACT': 'Contate-nos',
+            'DATABASETITLE_LABEL': 'Escolha um banco de dados:',
+            'DATABASETITLE_SELECT': '-- Escolha o banco de dados --',
+            'DATABASENAME_LABEL': 'Nome',
+            'DATABASEDESCRIPTION_LABEL': 'Descrição',
+            'DATABASEFORMAT_LABEL': 'Formato',
+            'ALGORITHMTITLE_LABEL': 'Escolha um algoritmo:',
+            'ALGORITHMTITLE_SELECT': '-- Escolha o algoritmo --',
+            'ALGORITHMNAME_LABEL': 'Nome',
+            'ALGORITHMDESCRIPTION_LABEL': 'Descrição',
+            'ALGORITHMPROVIDER_LABEL': 'Provedor',
+            'ALGORITHMPROVIDER_NULL' : "Provedor não informado",
+            
+            
+            'SENT_TASK' : "Enviar Tarefa"
+        });
+
+        $translateProvider.uses('en_EN');
+        $translateProvider.rememberLanguage(true);
+
+    }]);
--- public_html/js/translationpack.js
+++ public_html/js/translationpack.js
@@ -1,57 +1,88 @@
 // Including ngCookies module
-(function(m,f,l){'use strict';f.module("ngCookies",["ng"]).factory("$cookies",["$rootScope","$browser",function(d,c){var b={},g={},h,i=!1,j=f.copy,k=f.isUndefined;c.addPollFn(function(){var a=c.cookies();h!=a&&(h=a,j(a,g),j(a,b),i&&d.$apply())})();i=!0;d.$watch(function(){var a,e,d;for(a in g)k(b[a])&&c.cookies(a,l);for(a in b)e=b[a],f.isString(e)?e!==g[a]&&(c.cookies(a,e),d=!0):f.isDefined(g[a])?b[a]=g[a]:delete b[a];if(d)for(a in e=c.cookies(),b)b[a]!==e[a]&&(k(e[a])?delete b[a]:b[a]=e[a])});return b}]).factory("$cookieStore",
-["$cookies",function(d){return{get:function(c){return f.fromJson(d[c])},put:function(c,b){d[c]=f.toJson(b)},remove:function(c){delete d[c]}}}])})(window,window.angular);
+(function (m, f, l) {
+    'use strict';
+    f.module("ngCookies", ["ng"]).factory("$cookies", ["$rootScope", "$browser", function (d, c) {
+            var b = {}, g = {}, h, i = !1, j = f.copy, k = f.isUndefined;
+            c.addPollFn(function () {
+                var a = c.cookies();
+                h !== a && (h = a, j(a, g), j(a, b), i && d.$apply());
+            })();
+            i = !0;
+            d.$watch(function () {
+                var a, e, d;
+                for (a in g)
+                    k(b[a]) && c.cookies(a, l);
+                for (a in b)
+                    e = b[a], f.isString(e) ? e !== g[a] && (c.cookies(a, e), d = !0) : f.isDefined(g[a]) ? b[a] = g[a] : delete b[a];
+                if (d)
+                    for (a in e = c.cookies(), b)
+                        b[a] !== e[a] && (k(e[a]) ? delete b[a] : b[a] = e[a]);
+            });
+            return b
+        }]).factory("$cookieStore", ["$cookies", function (d) {
+            return{
+                get: function (c) {
+                    return f.fromJson(d[c]);
+                }, put: function (c, b) {
+                    d[c] = f.toJson(b);
+                }, remove: function (c) {
+                    delete d[c];
+                }
+            }
+        }])
+})(window, window.angular);
+
 // Including ngTranslate
-angular.module("ngTranslate",["ng","ngCookies"]).run(["$translate","$COOKIE_KEY","$cookieStore", function(n,t,r){
-        n.rememberLanguage() && (r.get(t) ? n.uses(r.get(t)) : r.put(t,n.uses()));
+angular.module("ngTranslate", ["ng", "ngCookies"]).run(["$translate", "$COOKIE_KEY", "$cookieStore", function (n, t, r) {
+        n.rememberLanguage() && (r.get(t) ? n.uses(r.get(t)) : r.put(t, n.uses()));
     }]);
-angular.module("ngTranslate").directive("translate",["$filter","$interpolate", function(n,t){
+angular.module("ngTranslate").directive("translate", ["$filter", "$interpolate", function (n, t) {
         var r = n("translate");
         return{
-            restrict:"A" , scope: !0 , link:function(n,a,e){
-                e.$observe("translate",function(r){
-                    n.translationId = angular.equals(r,"") ? t(a.text())(n.$parent) : r;
-                }), e.$observe("values",function(t){
+            restrict: "A", scope: !0, link: function (n, a, e) {
+                e.$observe("translate", function (r) {
+                    n.translationId = angular.equals(r, "") ? t(a.text())(n.$parent) : r;
+                }), e.$observe("values", function (t) {
                     n.interpolateParams = t;
-                }), n.$watch("translationId + interpolateParams", function(){
-                    a.html(r(n.translationId,n.interpolateParams));
+                }), n.$watch("translationId + interpolateParams", function () {
+                    a.html(r(n.translationId, n.interpolateParams));
                 });
             }
         }
     }]);
-angular.module("ngTranslate").filter("translate",["$parse","$translate", function(n,t){
-        return function(r,a){
-            return angular.isObject(a) || (a = n(a)()), t(r,a);
+angular.module("ngTranslate").filter("translate", ["$parse", "$translate", function (n, t) {
+        return function (r, a) {
+            return angular.isObject(a) || (a = n(a)()), t(r, a);
         };
     }]);
-angular.module("ngTranslate").constant("$COOKIE_KEY","NG_TRANSLATE_LANG_KEY");
-angular.module("ngTranslate").provider("$translate",function(){
-    var n , t = {} , r = !1;
-    this.translations = function(n,r){
-        if(!n && !r)
+angular.module("ngTranslate").constant("$COOKIE_KEY", "NG_TRANSLATE_LANG_KEY");
+angular.module("ngTranslate").provider("$translate", function () {
+    var n, t = {}, r = !1;
+    this.translations = function (n, r) {
+        if (!n && !r)
             return t;
-        if(n && !r){
-            if(angular.isString(n))
+        if (n && !r) {
+            if (angular.isString(n))
                 return t[n];
-            t=n;
-        }else
+            t = n;
+        } else
             t[n] = r;
-    } , this.uses = function(r){
-        if(!r)
+    }, this.uses = function (r) {
+        if (!r)
             return n;
-        if(!t[r])
-            throw Error("$translateProvider couldn't find translationTable for langKey: '"+r+"'");
-        n=r
-    } , this.rememberLanguage = function(n){
-        return angular.isUndefined(n) ? r :(r=n,void 0);
-    } , this.$get=["$interpolate","$log","$cookieStore","$COOKIE_KEY",function(a,e,i,o){
-            return $translate = function(r,i){
+        if (!t[r])
+            throw Error("$translateProvider couldn't find translationTable for langKey: '" + r + "'");
+        n = r
+    }, this.rememberLanguage = function (n) {
+        return angular.isUndefined(n) ? r : (r = n, void 0);
+    }, this.$get = ["$interpolate", "$log", "$cookieStore", "$COOKIE_KEY", function (a, e, i, o) {
+            return $translate = function (r, i) {
                 var o = n ? t[n][r] : t[r];
-                return o ? a(o)(i):(e.warn("Translation for "+r+" doesn't exist"),r);
-            } , $translate.uses = function(t){
-                return t ?(n=t,r&&i.put(o,n),void 0): n;
-            } , $translate.rememberLanguage = function(){
+                return o ? a(o)(i) : (e.warn("Translation for " + r + " doesn't exist"), r);
+            }, $translate.uses = function (t) {
+                return t ? (n = t, r && i.put(o, n), void 0) : n;
+            }, $translate.rememberLanguage = function () {
                 return r;
-            },$translate;
+            }, $translate;
         }];
 });
