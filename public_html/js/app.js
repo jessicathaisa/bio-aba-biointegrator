022f93d83c871617a14542eb828e7ddb5a4d0fac
Jéssica Thaisa Silva de Almeida <jalmeida@localhost.localdomain>
2015-06-18 03:12:27 -0300

    Buscando o resultado

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -11,20 +11,32 @@ function RunningTask($scope, $http, $localStorage, $timeout) {
     $scope.count = 0;
 
     var poll = function () {
-        $timeout(function () {
-            if ($scope.task === undefined || $scope.task.result === null) {
-                $scope.count++;
-                $http.get('http://localhost:9966/taskmanager-web/rest/biotask/' + $scope.lastTaskKey).
-                        success(function (data) {
-                            $scope.task = data;
-                        });
-                poll();
-            }
-        }, 1000);
+        $scope.count++;
+		findTask($scope.lastTaskKey)
+			.then(function(response){
+				$scope.task = response.data;
+				if($scope.task && $scope.task.status) {
+					retrieveResult($scope.lastTaskKey);
+				} else {
+			        $timeout(function () {
+						poll() 
+					}, 2000);;
+				}
+			});
     };
     poll();
-}
-;
+
+	function retrieveResult(taskId) {
+		return $http.get('http://localhost:9966/taskmanager-web/rest/biotaskresult/' + taskId)
+			.then(function(response) {
+				$scope.task.result = (response.data.result || "No result" );
+			});
+	}	
+
+	function findTask(taskId) {
+		return $http.get('http://localhost:9966/taskmanager-web/rest/biotask/' + taskId);
+	}
+};
 
 function BioTask($scope, $location, $http, $localStorage) {
     $scope.loadingDatabase = false;
